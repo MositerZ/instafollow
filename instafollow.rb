4c90977e01e18653cc5b2ea8165d24d216611a8a
DillonBordeleau <dillontbordeleau@gmail.com>
2013-11-25 09:53:15 -0500

    Text edit

--- instafollow.rb
+++ instafollow.rb
@@ -50,7 +50,6 @@ while 1 < 100
         user_count = user_count + 1
         puts "Followed Users:".green
         puts user_count
-<<<<<<< HEAD
           time2 = Time.now
         
 # sleeping program
@@ -91,51 +90,8 @@ while 1 < 100
           
           		access_tokens.rotate!
 
-=======
-        time2 = Time.now
-
-        # sleeping program
-        puts "Sleeping for 5 minutes."
-        puts "Current Time : " + time2.inspect
-        sleep 60
-        puts "4 minutes remaining."
-        sleep 60
-        puts "3 minutes remaining."
-        sleep 60
-        puts "2 minutes remaining."
-        sleep 60
-        puts "1 minute remaining."
-        sleep 60
-
-        # unfollowing each user
-        Instagram.unfollow_user(photo.user.id)
-        puts "Unfollowed User".yellow
-        puts "Following in 15 seconds..."
-        sleep 15
-
-      # rescuing if necessary
-      rescue Exception => e  
-        error_count = error_count + 1
-        puts e.message.red 
-        if error_count%2 == 0  
-          token = access_tokens.first 
-          puts "Error! Retry has failed, switching tokens #{token}".red
-
-          # rotating the access tokens to outsmart Instagram
-          Instagram.configure do |config|
-            config.access_token = token
-          end
-          access_tokens.rotate!
->>>>>>> c537b8f3f03911c69baaf938ea7e3129d968b348
         end
       end
     end
   end
 end
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> c537b8f3f03911c69baaf938ea7e3129d968b348
